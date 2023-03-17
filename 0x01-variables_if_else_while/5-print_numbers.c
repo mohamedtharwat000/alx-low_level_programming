4a7986ca74cf50d8a1febc89d90e7c9a91adc10e
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-17 23:42:11 +0200

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints single digits
+ * Return: 0
+ */
+int main(void)
+{
+char d;
+for (d = 0 ; d <= 9 ; d++)
+{
+  printf("%i", d);
+}
+putchar('\n');
+return (0);
+}
