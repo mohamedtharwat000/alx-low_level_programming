a64a87bd637ba550068c8549226c0816e3d06a0c
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-17 23:58:21 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,13 @@
 int main(void)
 {
 int c;
-for (c = '0x0'; c <= '0xf'; c++)
+for (c = '0'; c <= '9'; c++)
 {
-	putchar(c);
+putchar(c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
 }
 putchar('\n');
 return (0);
