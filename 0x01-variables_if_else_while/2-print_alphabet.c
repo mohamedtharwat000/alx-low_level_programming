dca60a37e2855c5c6a24b05cfef0613c5635f0d3
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-03-17 23:29:25 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 * main - print if the number is postive, zero, or negative
 *
 * Description: using the main function
-* this program prints "Programming is positive, zero, or negative
+* this program prints letters
 * Return: 0
 */
 int main(void)
