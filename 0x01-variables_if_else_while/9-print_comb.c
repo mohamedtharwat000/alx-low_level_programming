e7e9b606092d2953057013ce4f9f92abd19568d6
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-18 00:04:25 +0200

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program comb numbers
+ * Return: 0
+ */
+int main()
+{
+int c;
+int cc;
+for (c = '0'; c <= '9'; c++)
+{
+for (cc = '0'; cc <= '9'; cc++)
+{
+putchar(c);
+putchar(cc);
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
