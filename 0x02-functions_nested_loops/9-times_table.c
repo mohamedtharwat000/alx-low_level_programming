b533ec4b393605f525a8d9e79e8728ee0f50a315
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-22 04:42:07 +0200

    \9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,5 +6,35 @@
 
 void times_table(void)
 {
+	int row;
+	int column;
+	int number;
 
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			number = (row * column);
+
+			if (column == 0)
+			{
+				_putchar('0' + number);
+			}
+			else if (number <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + number);
+			}
+			else if (number > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (number / 10));
+				_putchar('0' + (number % 10));
+			}
+		}
+		_putchar('\n');
+	}
 }
