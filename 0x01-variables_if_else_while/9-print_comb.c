c9e11f1eac479e95e3a427a16ca608a9c81441fc
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-03-22 19:47:32 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print if the number is postive, zero, or negative
+ * main - comb numbers
  *
  * Description: using the main function
  * this program comb numbers
