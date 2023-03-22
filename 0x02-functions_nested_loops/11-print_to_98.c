22618c4a6ac85d4f1456610d8412d777fb26c97d
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-22 04:26:55 +0200

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,22 +7,22 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d", n);
-			printf(",");
-			printf(" ");
+			printf("%d, ", n);
 		}
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d", n);
-			printf(",");
-			printf(" ");
+			printf("%d, ", n);
 		}
+	}
+	else
+	{
+		printf("%d, ", n);
 	}
 }
