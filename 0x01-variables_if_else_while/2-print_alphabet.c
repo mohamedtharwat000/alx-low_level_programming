24088fa895a4281276fe89f789883332557f0acc
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-03-22 19:41:32 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - print if the number is postive, zero, or negative
+* main - prints alphabet letters
 *
 * Description: using the main function
 * this program prints letters
