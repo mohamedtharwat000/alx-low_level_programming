a25c000ca817eccf8d32af66817a5963791a69a3
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-22 03:02:38 +0200

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,17 +6,14 @@
  * Return: last digit of number
  */
 
-int print_last_digit(int c)
+int print_last_digit(int n)
 {
-	if (c > 0 || c == 0)
+	if (n < 0)
 	{
-	_putchar (c % 10 + '0');
-	return (c % 10);
+		n *= -1;
+		_putchar('0' + n % 10);
+		return (n % 10);
 	}
-	else
-	{
-	c = c * -1;
-	_putchar (c % 10 + '0');
-	return (c % 10);
-	}
+	_putchar('0' + n % 10);
+	return (n % 10);
 }
