181644fc79f1462b739f1a5a5afc67c529f0069c
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-22 02:56:51 +0200

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,9 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		n *= -1;
-		_putchar(n % 10 + '0');
+		_putchar('0' + n % 10);
 		return (n % 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar('0' + n % 10);
 	return (n % 10);
 }
