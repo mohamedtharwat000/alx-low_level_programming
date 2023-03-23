e389c760a704ef9ed376954fdc0c1a216211a01d
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-24 00:56:00 +0200

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,15 @@
 
 void print_most_numbers(void)
 {
-	int n = 0;
+	int n = '0';
 
-	while (n <= 9)
+	while (n <= '9')
 	{
-		if (n == 2 || n == 4)
+		if (n == '2' || n == '4')
 		{
 			continue;
 		}
-		_putchar(n + '0');
+		_putchar(n);
 		n++;
 	}
 _putchar('\n');
