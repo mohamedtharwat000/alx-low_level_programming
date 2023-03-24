7e0e33429bd613b5bac7889eaf6a3dfdcb9964e5
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-24 04:07:27 +0200

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,14 @@ int main(void)
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			if (n == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
 		}
 		else if (n % 3 == 0 && n % 5 == 0)
 		{
