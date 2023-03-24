1a61de2593f51615053779dcd3d05d00bf0a376c
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-24 04:02:04 +0200

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - “Fizz-Buzz test”
+ * Return: void
+*/
+
+void main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%i ", n);
+		}
+		n++;
+	}
+	printf("\n");
+}
