6c65e2f5b95ad11c850a136a6fe65b1cb98874bb
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-31 00:54:52 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,21 +10,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, srclen;
+	int i;
 
-	for (i = 0; src[i]; i++)
-	{
-		srclen = i + 1;
-	}
-
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	for (i = srclen; i < n; i++)
+	while (i < n)
 	{
 		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
