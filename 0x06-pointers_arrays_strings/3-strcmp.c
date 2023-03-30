e1db05af8525c0f04ac709439dc2d55e32952ddc
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-31 01:20:44 +0200

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: string 1.
+ * @s2: string 2.
+ * Return: 0 if two are equal.
+ * + if 1 > 2
+ * - if 2 > 1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int result = 0;
+
+	for ( i = 0; s1[i] || s2[i]; i++)
+	{
+		if (s1[i] < s2[i])
+		{
+			result++;
+		}
+		else if (s1[i] > s2[i])
+		{
+			result--;
+		}
+		else
+		{
+			result = result;
+		}
+	}
+
+
+	return (result);
+}
