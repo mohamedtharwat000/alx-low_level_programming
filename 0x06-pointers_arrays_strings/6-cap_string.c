9e4b78e664a181519a75ed95dbd3ee0c7f50c2d5
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-31 03:21:05 +0200

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string.
+ * @str: input string.
+ * Return: capitalize string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' ||
+			str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
+			str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			if (str[i] && str[i] >= 97 && str[i] <= 122)
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+
+	}
+	return (str);
+}
