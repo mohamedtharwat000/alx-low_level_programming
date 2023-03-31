36b25d42f9484255502af50576c3a37788f4ef4c
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-31 03:25:13 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,15 +12,18 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i]; i++)
 	{
-		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' ||
-			str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
-			str[i - 1] == '{' || str[i - 1] == '}')
+		if (
+			(i == 0 || str[i - 1] == ' '
+			|| str[i - 1] == '\t' || str[i - 1] == '\n'
+			|| str[i - 1] == ',' || str[i - 1] == ';'
+			|| str[i - 1] == '.' || str[i - 1] == '!'
+			|| str[i - 1] == '?' || str[i - 1] == '"'
+			|| str[i - 1] == '(' || str[i - 1] == ')'
+			|| str[i - 1] == '{' || str[i - 1] == '}')
+			&& str[i] >= 97 && str[i] <= 122
+			)
 		{
-			if (str[i] && str[i] >= 97 && str[i] <= 122)
-			{
-				str[i] = str[i] - 32;
-			}
+			str[i] = str[i] - 32;
 		}
 
 	}
