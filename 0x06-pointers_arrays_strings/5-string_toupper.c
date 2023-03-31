56bf85ed6baf8c1e7985a423175395a24b1239af
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-31 02:54:46 +0200

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reverse_array - function that reverses the content of an array of integers.
- * @a: array.
- * @n: n is the number of elements of the array.
+ * string_toupper - function that changes all lowercase letters to uppercase.
+ * @str: input string.
+ * Return: upper casr string
  */
 
 char *string_toupper(char *str)
