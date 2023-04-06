972332c9fcb60c8ffdf7be8b59717fd9c5160dfd
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-06 03:41:58 +0200

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number.
+ * raised to the power of y.
+ * @n: input number
+ * Return: the natural square root of a number.
+*/
+
+int sqrt_helper(int n, int i)
+{
+	if (i * i > n)
+	{
+		return i - 1;
+	}
+	return sqrt_helper(n, i + 1);
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return -1;
+	}
+
+	if (n == 0 || n == 1)
+	{
+		return n;
+	}
+
+	return sqrt_helper(n, 1);
+}
