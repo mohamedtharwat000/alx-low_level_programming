f18cbc3f1273f4c9ed24309605ebe4fd06579eca
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-06 03:52:57 +0200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,27 +5,35 @@
  * raised to the power of y.
  * @n: input number
  * Return: the natural square root of a number.
+ *
 */
-
-int sqrt_helper(int n, int i)
-{
-	if (i * i > n)
-	{
-		return i - 1;
-	}
-	return sqrt_helper(n, i + 1);
-}
 
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
 	else if (n == 0 || n == 1)
 	{
-		return n;
+		return (n);
 	}
 
-	return sqrt_helper(n, 1);
+	return (sqrt_helper(n, 1));
+}
+
+/**
+ * sqrt_helper - helper function
+ * @n: helper input
+ * @i: helper input
+ * Return: the natural square root of a number.
+ */
+
+int sqrt_helper(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (i - 1);
+	}
+	return (sqrt_helper(n, i + 1));
 }
