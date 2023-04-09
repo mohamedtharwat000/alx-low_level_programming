3c07a1a41bb806c17981527e6dbe26f7db2d142f
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-09 21:00:59 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * main - program that prints its name, followed by a new line.
@@ -7,7 +8,7 @@
 * Return: 0
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	printf("%s\n", *argv);
 	return (0);
