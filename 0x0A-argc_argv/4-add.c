ee899a64a6d1abb6f17849ea3d8021ae997cc060
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-10 03:45:04 +0200

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,9 +16,9 @@ int main(int argc, char **argv)
 	for (i = 1; i < argc; i++)
 	{
 
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; *(*(argv + i) + j) != '\0'; j++)
 		{
-			if ((argv[i][j] > '9' || argv[i][j] < '0'))
+			if (*(*(argv + i) + j) < '0' || *(*(argv + i) + j) > '9')
 			{
 				printf("Error\n");
 				return (1);
