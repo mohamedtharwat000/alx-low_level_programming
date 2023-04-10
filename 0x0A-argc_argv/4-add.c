eac1ad4e475c990bc0879694813f71059d5f394d
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-10 03:23:23 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,12 +13,6 @@ int main(int argc, char **argv)
 {
 	int i, result = 0;
 
-	if (argc < 2)
-	{
-		printf("0\n");
-		return (0);
-	}
-
 	for (i = 1; i < argc; i++)
 	{
 		if (atoi(*(argv + i)) == 0 && **(argv + i) != '0')
