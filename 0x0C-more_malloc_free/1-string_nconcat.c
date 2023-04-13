1a4951bc015114679af785a58485010aa1653ba1
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-14 00:40:50 +0200

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - function that concatenates two strings.
+ * @s1: str 1
+ * @s2: str 2
+ * @n: first n bytes of s2
+ * Return: pointer shall point to a newly allocated space in memory,
+ * which contains s1, followed by the first n bytes of s2,
+ * and null terminated
+ * If the function fails, it should return NULL
+ * If n is greater or equal to the length of s2 then use the entire string s2
+ * if NULL is passed, treat it as an empty string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, s1len, s2len;
+	char *str;
+	
+	s1len = 0;
+	s2len = 0;
+	
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	
+	for (i = 0; *(s1 + i); i++)
+	{
+		s1len++;
+	}
+	
+	for (i = 0; *(s2 + i); i++)
+	{
+		s2len++;
+	}
+	
+	if (n > s2len)
+	{
+		n = s2len;
+	}
+	
+	str = malloc(s1len + n);
+	
+	if (!str)
+	{
+		return (NULL);
+	}
+	
+	for (i = 0; i < s1len; i++)
+	{
+		str[i] = s1[i];
+	}
+	
+	for (; i < s1len + n; i++)
+	{
+		str[i] = s2[(n + i) - (s1len + n)];
+	}
+	
+	str[i+1] = '\0';
+	
+	return (str);
+}
