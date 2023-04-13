f343cf231a18bd72972c8dac6a08d80ebd0a930c
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-14 00:50:45 +0200

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,55 +16,42 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, s1len, s2len;
+	int i, s1len = 0, s2len = 0;
 	char *str;
-	
-	s1len = 0;
-	s2len = 0;
-	
+
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	
-	if (s2 == NULL)
+	else if (s2 == NULL)
 	{
 		s2 = "";
 	}
-	
 	for (i = 0; *(s1 + i); i++)
 	{
 		s1len++;
 	}
-	
 	for (i = 0; *(s2 + i); i++)
 	{
 		s2len++;
 	}
-	
 	if (n > s2len)
 	{
 		n = s2len;
 	}
-	
 	str = malloc(s1len + n);
-	
 	if (!str)
 	{
 		return (NULL);
 	}
-	
 	for (i = 0; i < s1len; i++)
 	{
 		str[i] = s1[i];
 	}
-	
 	for (; i < s1len + n; i++)
 	{
 		str[i] = s2[(n + i) - (s1len + n)];
 	}
-	
-	str[i+1] = '\0';
-	
+	str[i + 1] = '\0';
 	return (str);
 }
