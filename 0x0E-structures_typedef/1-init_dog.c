23c2ec562dadce2cca65001451fb53c2b557e94c
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-18 04:03:26 +0200

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,6 +10,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	dog dog;
+
+	if (d == NULL)
+	{
+		d = &dog;
+	}
+	
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
