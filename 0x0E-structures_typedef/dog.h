b25cf2f976b3a2225bafcdd51733fc7e0bb4b073
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-18 03:49:13 +0200

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/**
+ * struct dog - dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif /* HEADER_H */
