64b3b902982e5a7d975bb4833fa2eb09b2a6ef39
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-19 22:58:39 +0200

    forget betty on header haha

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * print_name - function that prints a name.
@@ -9,7 +8,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (!name || !f)
 	{
 		return;
 	}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,4 +4,4 @@
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
-#endif // HEADER_H
+#endif /* HEADER_H */
