a51eca9428ea51a9e08c90f9a7e82e31aab75a79
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-04-29 16:50:26 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	*next = *head;
+	next = *head;
 	while (next->next)
 	{
 		if (current_index + 1 == idx)
