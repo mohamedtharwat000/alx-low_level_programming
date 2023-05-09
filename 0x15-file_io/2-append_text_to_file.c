4aac59ef6cb4f5e2c8fcb71a8cedbe052d5ebf01
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-05-09 05:56:41 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,5 +17,29 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, wr;
 
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_RDWR | O_APPEND);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		wr = write(fd, text_content, strlen(text_content));
+		if (wr == -1)
+		{
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
 }
