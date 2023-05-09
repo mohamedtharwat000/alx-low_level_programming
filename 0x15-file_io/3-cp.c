a7744a1b873f9d720f437fd3aba6059077e3d8bd
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-05-09 07:39:17 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void close_fd(int fd);
+
 /**
  * main - program that copies the content of a file to another file.
  *
@@ -11,5 +13,68 @@
 
 int main(int argc, char **argv)
 {
+	int f_from, f_to, readed;
+	char *file_from = argv[1], *file_to = argv[2];
+	char buffer[1024];
 
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	f_from = open(file_from, O_RDONLY);
+	if (f_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	f_to = open(file_to, O_CREAT | O_EXCL | O_WRONLY, 0664);
+	if (f_to == -1)
+	{
+		f_to = open(file_to, O_WRONLY | O_TRUNC);
+		if (f_to == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+
+	while (readed != 0)
+	{
+		if (readed == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			close_fd(f_from);
+			close_fd(f_to);
+			exit(98);
+		}
+		if (readed != write(f_to, buffer, readed))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			close_fd(f_from);
+			close_fd(f_to);
+			exit(99);
+		}
+		readed = read(f_from, buffer, 1024);
+	}
+
+	close_fd(f_from);
+	close_fd(f_to);
+	return (0);
+}
+
+/**
+ * close_fd - close_fd
+ * @fd: file descriptor
+ */
+
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
