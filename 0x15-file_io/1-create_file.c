986cb6bf35a4f3a238270129b690343d1abea4d0
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-05-09 06:03:27 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
+	fd = open(filename, O_EXCL | O_CREAT | O_WRONLY, 0600);
 
 	if (fd == -1)
 	{
@@ -37,15 +37,14 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		if (!text_content)
+		if (text_content)
 		{
-			return (1);
-		}
-		wr = write(fd, text_content, strlen(text_content));
-		if (wr <= 0)
-		{
-			close(fd);
-			return (-1);
+			wr = write(fd, text_content, strlen(text_content));
+			if (wr <= 0)
+			{
+				close(fd);
+				return (-1);
+			}
 		}
 	}
 
