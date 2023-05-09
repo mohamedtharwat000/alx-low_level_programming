bc1a23f5157e5c0a534b8681d191f9e9eed5de04
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-05-09 06:07:46 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -40,7 +40,7 @@ int create_file(const char *filename, char *text_content)
 		if (text_content)
 		{
 			wr = write(fd, text_content, strlen(text_content));
-			if (wr <= 0)
+			if (wr == -1)
 			{
 				close(fd);
 				return (-1);
