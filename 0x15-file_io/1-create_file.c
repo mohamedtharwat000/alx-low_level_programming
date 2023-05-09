011c57a223175132110cbfc55bd6d57d7bc1251a
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-05-09 05:36:40 +0300

    test 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,15 +27,24 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_EXCL, O_WRONLY, 0600);
+
 	if (fd == -1)
 	{
-		return (-1);
+		fd = open(filename, O_TRUNC);
+		if (fd == -1)
+		{
+			return (-1);
+		}
 	}
-
-	if (text_content)
+	else
 	{
-		len = _strlen(text_content);
+		if (!text_content)
+		{
+			close(fd);
+			return (1);
+		}
+		len = strlen(text_content);
 		wr = write(fd, text_content, len);
 		if (wr <= 0)
 		{
@@ -47,20 +56,3 @@ int create_file(const char *filename, char *text_content)
 	return (1);
 }
 
-/**
- * _strlen - function that get string length
- *
- * @str: string
- *
- * Return: string length
- */
-
-int _strlen(char *str)
-{
-	int len = 0;
-
-	while (str[len++])
-	{
-	}
-	return (len);
-}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
