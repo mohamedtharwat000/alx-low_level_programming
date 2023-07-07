70aaca3c152b30fe482cd502080c6789b822f87c
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-07 14:45:17 +0300

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,10 +11,10 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i = 0;
 	hash_node_t *node = NULL;
 
-if (!ht)
-{
-  return;
-}
+        if (ht == NULL) 
+        { 
+               return; 
+        }
 
 	printf("{");
 
