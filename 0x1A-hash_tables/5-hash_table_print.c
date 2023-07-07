76088e9628688e1cafc0eee90ca7dcad8404581d
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-07 03:10:38 +0300

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ *
+ * @ht: the hash table you want to print
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node = NULL;
+
+	printf("{");
+
+	for (; i < ht->size; i++)
+	{
+		if (ht->array[i] == NULL)
+		{
+			continue;
+		}
+		node = ht->array[i];
+		printf("'%s': '%s'", node->key, node->value);
+		i++;
+		break;
+	}
+
+	for (; i < ht->size; i++)
+	{
+		if (ht->array[i] == NULL)
+		{
+			continue;
+		}
+		node = ht->array[i];
+		while (node)
+		{
+			printf(", '%s': '%s'", node->key, node->value);
+			node = node->next;
+		}
+	}
+
+	printf("}\n");
+}
