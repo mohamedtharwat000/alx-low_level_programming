9769311198d77ad9f695b9eacce06bdde6d254cb
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-25 04:34:57 +0300

    Create 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+function def island_perimeter(grid):
+that returns the perimeter of the island described in grid.
+"""
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid
+
+        Args:
+            grid (list): list of lists of integers
+
+        Returns:
+            int: perimeter of island
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
