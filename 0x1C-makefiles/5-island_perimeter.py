f1dc00281e72a336a662586b47279ab9ceb5a189
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-25 04:36:05 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,6 +5,7 @@ function def island_perimeter(grid):
 that returns the perimeter of the island described in grid.
 """
 
+
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid
 
